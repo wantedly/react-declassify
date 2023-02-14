145a27a2bda6d575034ec7715038a3d8f4ce1554
Masaki Hara <ackie.h.gmai@gmail.com>
2023-02-14 16:06:14 +0900

    Add @codemod/core

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,18 @@
 import { expect, test } from "@jest/globals";
+import { transform } from "@codemod/core";
 
 type _T = 42;
 
 test("example", () => {
   expect(42).toBe(42);
+});
+
+test("recast transform", () => {
+  const result = transform("a ?? b", {
+    configFile: false,
+    babelrc: false,
+    plugins: [],
+  })
+
+  expect(result.code).toBe("a ?? b");
 });
