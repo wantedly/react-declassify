726e907df06e3db9cb4736fc6cdb0f37219e6184
Masaki Hara <ackie.h.gmai@gmail.com>
2023-02-14 16:30:17 +0900

    Implement class detection

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,13 +1,22 @@
-import { expect, test } from "@jest/globals";
-import { transform } from "@codemod/core";
+import { describe, it, expect } from "@jest/globals";
+import { transform as transformCore } from "@codemod/core";
 import plugin from "./index.js";
 
-test("recast transform", () => {
-  const result = transform("a ?? b", {
+function transform(code: string) {
+  const result = transformCore(code, {
     configFile: false,
     babelrc: false,
     plugins: [plugin],
-  })
+  });
+  return result.code;
+}
 
-  expect(result.code).toBe("a ?? b");
+describe("react-unclass", () => {
+  it("transforms Component class", () => {
+    expect(transform("class C extends Component {}")).toBe("const foo = null;");
+  });
+
+  it("ignores plain classes", () => {
+    expect(transform("class C {}")).toBe("class C {}");
+  });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,34 @@
-import type { PluginObj } from "@babel/core";
+import type { NodePath, PluginObj } from "@babel/core";
+import type { ClassDeclaration, TSType } from "@babel/types";
 export default function plugin(babel: typeof import("@babel/core")): PluginObj {
+  const { types: t } = babel;
   return {
     name: "react-unclass",
-    visitor: {},
+    visitor: {
+      ClassDeclaration(path) {
+        const head = analyzeHead(path);
+        if (!head) {
+          return;
+        }
+        path.replaceWith(t.variableDeclaration("const", [
+          t.variableDeclarator(
+            t.identifier("foo"),
+            t.nullLiteral(),
+          )
+        ]));
+      },
+    },
   };
+}
+
+type ComponentHead = {
+  props: NodePath<TSType> | undefined;
+};
+
+function analyzeHead(path: NodePath<ClassDeclaration>): ComponentHead | undefined {
+  const superClass = path.get("superClass");
+  if (!superClass) return;
+  if (superClass.isIdentifier() && superClass.node.name === "Component") {
+    return { props: undefined };
+  }
 }
